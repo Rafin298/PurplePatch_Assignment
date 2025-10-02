5546e5ba7a2d8728d3cee3ca6837867003e6a305
Rafin298 <rafinkhan298@gmail.com>
2025-10-02 21:29:13 +0600

    First objective Completed

---
+++ objectiveOne.py
@@ -0,0 +1,34 @@
+## Check a publisher’s/ads.txt file and verify whether PurplePatch’s seller ID is included.
+
+import os
+
+PURPLEPATCH_ID = "purplepatch.online"
+
+def parse_ads_txt(file_path):
+    purplepatch_found = False
+    competitors = []
+
+    with open(file_path, "r", encoding="utf-8") as f:
+        for line in f:
+            line = line.strip()
+
+            parts = [p.strip() for p in line.split(",")]
+            if len(parts) >= 2:
+                pub_id =  parts[1]
+
+                if PURPLEPATCH_ID in pub_id:
+                    purplepatch_found = True
+
+    return purplepatch_found, list(set(competitors))
+
+
+report = {}
+ads_folder = "Ads.txt"
+for fname in os.listdir(ads_folder):
+    fpath = os.path.join(ads_folder, fname)
+    purplepatch_found, competitors = parse_ads_txt(fpath)
+    report[fname] = {
+        "purplepatch_present": purplepatch_found,
+    }
+    
+    print(fname,"------------>",report[fname])
